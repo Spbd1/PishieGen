2849c9fad6a477b4f47492e2f6b661063e90a781
Spbd1 <148923621+Spbd1@users.noreply.github.com>
2026-05-07 02:47:10 +0000

    Add trait tradeoff registry

--- pishiegen/fitness/__init__.py
+++ pishiegen/fitness/__init__.py
@@ -1,5 +1,12 @@
 """Fitness scoring for biome-specific adaptation."""
 
 from pishiegen.fitness.scoring import FitnessResult, score_fitness
+from pishiegen.fitness.tradeoffs import TRAIT_TRADEOFFS, TraitEffect, all_trait_effects
 
-__all__ = ["FitnessResult", "score_fitness"]
+__all__ = [
+    "FitnessResult",
+    "TRAIT_TRADEOFFS",
+    "TraitEffect",
+    "all_trait_effects",
+    "score_fitness",
+]
